52d84569e75e3a9b523b1ff28bcef29d4753f45f
cevazquez <carlvazquez498@gmail.com>
2019-02-16 12:26:49 -0700

    Add banner image to header

--- src/shared/Header/index.js
+++ src/shared/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './header.css';
+import './Header.css';
 
 export function Header() {
     return (
         <div>
-            <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+            <nav className='navbar bg-dark navbar-expand-lg'>
                 <div className='container'>
                     <a className='navbar-brand text-uppercase' href=''>Crash Pads</a>
                     <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
@@ -18,12 +18,23 @@ export function Header() {
                     </div>
                 </div>
             </nav>
-            <div id='banner'>
-                <form className='form-inline my-2 my-lg-0'>
+
+            <section className="clearfix homeBanner">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <div className="bannerInfo">
+                                <h1>This is the title.</h1>
+                                <p>This app will eventually be something.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <form className='form-inline my-2 my-lg-0'>
                     <input className='form-control mr-sm-2 search-bar' type='search' placeholder='ex. New York City' aria-label='Search'></input>
                     <button className='btn btn-outline-success my-2 my-sm-0 btn-search' type='submit'>Search</button>
                 </form>
-            </div>
+                </div>
+            </section>
         </div>
     )
 }
